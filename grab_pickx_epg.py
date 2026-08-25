c2a78d6da04af2ce3d4a19e845a1a4b46e4a6a9b
Ayoub Boukous <37737944+ayoubboukous27@users.noreply.github.com>
2026-08-25 10:08:08 +0100

    Fix command option from 'site' to 'sites'

--- grab_pickx_epg.py
+++ grab_pickx_epg.py
@@ -12,7 +12,7 @@ Ce script :
   1. Clone (ou met à jour) un checkout shallow de iptv-org/epg dans un
      dossier de travail.
   2. Installe les dépendances npm (une seule fois).
-  3. Lance `npm run grab -- --site=pickx.be [--lang=xx] [--days=N]`.
+  3. Lance `npm run grab -- --sites=pickx.be [--lang=xx] [--days=N]`.
   4. Copie le guide.xml généré vers le chemin de sortie demandé.
 
 Utilisation :
